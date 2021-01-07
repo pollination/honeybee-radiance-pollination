49f87968f02f574589597cda112026199b80ecb7
Mostapha <sadeghipour@gmail.com>
2021-01-06 19:34:55 -0500

    test: add tests

---
+++ tests/matrix_test.py
@@ -0,0 +1,8 @@
+from pollination_honeybee_radiance.matrix import MatrixMultiplication
+from queenbee.plugin.function import Function
+
+
+def test_matrix_multiplication():
+    function = MatrixMultiplication().queenbee
+    assert function.name == 'matrix-multiplication'
+    assert isinstance(function, Function)
