6e153555ad77aae646acbb16f4439559d1dad56f
Mostapha <sadeghipour@gmail.com>
2021-01-28 19:36:12 -0500

    fix: update packaging to new version of dsl

--- pollination_honeybee_radiance/__init__.py
+++ pollination_honeybee_radiance/__init__.py
@@ -1,24 +1,9 @@
 """Honeybee Radiance plugin for Pollination."""
 
 __queenbee__ = {
-    'name': 'honeybee-radiance',
-    'description': 'Honeybee Radiance plugin for Pollination.',
+    'name': 'honeybee-radiance',  # name for queenbee package - this will overwrite the Python package name
     'icon': 'https://raw.githubusercontent.com/ladybug-tools/artwork/master/icons_bugs/grasshopper_tabs/HB-Radiance.png',
-    'home': 'https://ladybug.tools/honeybee-radiance/docs',
-    'sources': [
-        'https://github.com/ladybug-tools/honeybee-radiance',
-        'https://hub.docker.com/r/ladybugtools/honeybee-radiance'
-    ],
-    'tag': '0.4.0',  # tag for honeybee-radiance plugin
     'app_version': '5.4',  # tag for version of Radiance
-    'keywords': ['honeybee', 'radiance', 'ladybug-tools', 'daylight'],
-    'maintainers': [
-        {'name': 'mostapha', 'email': 'mostapha@ladybug.tools'}
-    ],
-    'license': {
-        'name': 'PolyForm Shield License 1.0.0',
-        'url': 'https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt'
-    },
     'config': {
         'docker': {
             'image': 'ladybugtools/honeybee-radiance:1.28.12',
