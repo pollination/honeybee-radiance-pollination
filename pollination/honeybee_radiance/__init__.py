ac8b8a2fe8e9b9f75e1f0b9ff676948bbc6783be
Mostapha <sadeghipour@gmail.com>
2021-02-05 20:15:09 -0500

    refactor: use an updated function to get the docker image tag

--- pollination/honeybee_radiance/__init__.py
+++ pollination/honeybee_radiance/__init__.py
@@ -1,10 +1,11 @@
 """Honeybee Radiance plugin for Pollination."""
-from pollination_dsl.package import get_requirement_version
+from pollination_dsl.common import get_docker_image_from_dependency
 
 # set the version for docker image dynamically based on honeybee-radiance version
 # in dependencies
-image_version = get_requirement_version(__package__, 'honeybee-radiance')
-image_id = f'ladybugtools/honeybee-radiance:{image_version}'
+image_id = get_docker_image_from_dependency(
+    __package__, 'honeybee-radiance', 'ladybugtools'
+)
 
 __pollination__ = {
     'app_version': '5.4',  # optional - tag for version of Radiance
