e98afeb9139f8ce966e9bcb7c0cbee65a9c503cf
Mostapha <sadeghipour@gmail.com>
2021-03-23 23:20:33 -0400

    fix(parse sun up hours): add support for leap year
    also add support for shorter timestep

--- pollination/honeybee_radiance/sun.py
+++ pollination/honeybee_radiance/sun.py
@@ -40,9 +40,24 @@ class ParseSunUpHours(Function):
         description='Path to sun-modifiers file.', path='suns.mod'
     )
 
+    timestep = Inputs.int(
+        description='An integer value to set the timestep of the input hours. If '
+        'timestep is set to 1 the time will be offset by -0.5 to align with the start '
+        'of the hour. For other timesteps the hour will not be adjusted.', default=1,
+        spec={'type': 'integer', 'minimum': 1}
+    )
+
+    leap_year = Inputs.str(
+        description='A flag to switch between a normal year and a leap year. The '
+        'default is a normal year. Use leap-year if the input hours are for a full '
+        'year', default='full-year',
+        spec={'type': 'string', 'enum': ['full-year', 'leap-year']}
+    )
+
     @command
     def parse_hours(self):
-        return 'honeybee-radiance sunpath parse-hours suns.mod --name sun-up-hours.txt'
+        return 'honeybee-radiance sunpath parse-hours suns.mod ' \
+            '--name sun-up-hours.txt --timestep {{self.timestep}} --{{self.leap_year}}'
 
     sun_up_hours = Outputs.file(
         description='A text file that includes all the sun up hours. Each hour is '
