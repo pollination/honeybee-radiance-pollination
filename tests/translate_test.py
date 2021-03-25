360cc4db997bde8f38d096ca54103c5f6b74a1aa
Chris Mackey <chris@ladybug.tools>
2021-03-25 12:46:15 -0400

    fix(translate): Update model translate command to include optional check

--- tests/translate_test.py
+++ tests/translate_test.py
@@ -1,12 +1,18 @@
-from pollination.honeybee_radiance.translate import CreateRadianceFolder, \
-    CreateRadiantEnclosureInfo
+from pollination.honeybee_radiance.translate import CreateRadianceFolderGrid, \
+    CreateRadianceFolderView, CreateRadiantEnclosureInfo
 
 from queenbee.plugin.function import Function
 
 
-def test_create_radiance_folder():
-    function = CreateRadianceFolder().queenbee
-    assert function.name == 'create-radiance-folder'
+def test_create_radiance_folder_grid():
+    function = CreateRadianceFolderGrid().queenbee
+    assert function.name == 'create-radiance-folder-grid'
+    assert isinstance(function, Function)
+
+
+def test_create_radiance_folder_view():
+    function = CreateRadianceFolderView().queenbee
+    assert function.name == 'create-radiance-folder-view'
     assert isinstance(function, Function)
 
 
