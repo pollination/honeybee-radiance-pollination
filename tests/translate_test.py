b040d4e01a7b76f404347ee568959b60927e0235
Mostapha <sadeghipour@gmail.com>
2021-03-28 14:33:33 -0400

    tests: add test

--- tests/translate_test.py
+++ tests/translate_test.py
@@ -1,7 +1,13 @@
 from pollination.honeybee_radiance.translate import CreateRadianceFolderGrid, \
-    CreateRadianceFolderView, CreateRadiantEnclosureInfo
+    CreateRadianceFolderView, CreateRadiantEnclosureInfo, CreateRadianceFolder
 
 from queenbee.plugin.function import Function
+
+
+def test_create_radiance_folder():
+    function = CreateRadianceFolder().queenbee
+    assert function.name == 'create-radiance-folder'
+    assert isinstance(function, Function)
 
 
 def test_create_radiance_folder_grid():
